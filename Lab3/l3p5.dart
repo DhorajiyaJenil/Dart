7c67608a4d3ea20f9bc39d39cca874e16a0da955
DhorajiyaJenil <121678596+DhorajiyaJenil@users.noreply.github.com>
2022-12-30 19:49:17 +0530

    Add files via upload

---
+++ Lab3/l3p5.dart
@@ -0,0 +1,15 @@
+
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Enter String");
+  String s = stdin.readLineSync()!;
+
+  for(int i=s.length-1;i>=0;i--)
+  {
+    print("${s[i]}");
+  }
+
+  //print("${s.split('').reversed.toString()}");
+  
+}
